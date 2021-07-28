b1cc74b2a92c38c985a3678d98ee3445ad4dadc1
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-07-28 13:58:01 -0400

    moved connect function to client.js file and imported the function to play.js

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', function(data) {
+    console.log(data);
+  })
+  return conn;
+};
+module.exports = {connect};
